687dd9571f2a8bff0d4e7ea59edeaa3c8386c59c
EnchiladaStyle <jon19073@byui.edu>
2024-09-06 13:18:46 -0600

    Solved longest palindrome problem

---
+++ longestPalindrome.py
@@ -0,0 +1,32 @@
+def longestPalindrome(s):
+    
+    longest = ""
+    current = ""
+
+    for i in range(len(s)):
+        leftPointer = i
+        rightPointer = i
+
+        while leftPointer >= 0 and rightPointer < len(s) and s[leftPointer] == s[rightPointer]:
+            current = s[leftPointer:rightPointer+1]
+            if len(current) > len(longest):
+                longest = current
+            leftPointer -= 1
+            rightPointer += 1
+
+        leftPointer = i
+        rightPointer = i+1
+
+        while leftPointer >= 0 and rightPointer < len(s) and s[leftPointer] == s[rightPointer]:
+            current = s[leftPointer:rightPointer+1]
+            if len(current) > len(longest):
+                longest = current
+            leftPointer -= 1
+            rightPointer += 1
+
+    return longest
+
+string = "stanleyl yelnats jijijij"
+
+print(longestPalindrome(string))
+
